aacb6d019a06bdebff65bbefa82218f9638cb0ff
Nathan-spec <Nathankkemboi@gmail.com>
2022-03-16 05:13:24 -0700

    Nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
-/** 
- * main - Entry position
- * write a program that prints Holberton, followed by a new line
- * Return:Always(Success)
+
+/**
+ *main - Entry point
+ *write a program that prints Holberton, followed by a new line.
+ * Return: Always (Success)
  */
 int main(void)
 {
-	char*ch = "_putchar";
+	char *ch = "_putchar";
+
 
 	while (*ch)
-	{
-		_putchar(*ch);
-		ch++;
-	}
+{
+	_putchar(*ch);
+	ch++;
+}
 	_putchar('\n');
-return(0);
+	return (0);
 }
