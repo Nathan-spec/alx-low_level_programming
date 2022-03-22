ec1070b6ba4cdf5d8850bfb7b74b8069af7cacdc
Nathan-spec <Nathankkemboi@gmail.com>
2022-03-22 08:12:35 -0700

    Second half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 	for (length = 0; str[length] != '\0'; length++)
 	{
 	}
-	n = (length - 1)/2;
+	n = (length - 1) / 2;
 	for (i = n + 1; str[i] != '\0'; i++)
 	{
 		putchar(str[i]);
