783b456bb26b14e5122e2d15b2f0596e1285fd7c
Nathan-spec <Nathankkemboi@gmail.com>
2022-03-23 05:26:28 -0700

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/** 
+ *strcat - Concatantes two strings.
+ *@*dest - Pointer to the destination array.
+ *@*src - String to be appended.
+ */
+char *_strcat(char *dest, char *src)
+{
+	*dest = s2;
+	*src = s1;
+}
