97b67de83a8098a21c5b8443d7dfbb8d66b5d338
Nathan-spec <Nathankkemboi@gmail.com>
2022-03-24 08:02:08 -0700

    Create 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,27 @@
 
 /**
  * _strncat - Concantenates two strings.
- * @dest - Pointer to the destination array.
- * @src - String to be appended
+ * @dest:Pointer to the destination array.
+ * @src:String to be appended.
+ *
+ * @n: number of bytes to be concatenated.
+ *
+ * Return : pointer to be concatenated.
  */
-char *_strncat(char 
+char *_strncat(char *dest, char *src, int n)
+{
+	int length, j;
+
+	length = 0;
+
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++; length++)
+	{
+		dest[length] = src[j];
+	}
+	dest[length] = '\0';
+	return (dest);
+}
