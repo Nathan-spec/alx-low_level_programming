bb7fc7839e9ea3ab43d98fa872ca32a2de00dfc3
Nathan-spec <Nathankkemboi@gmail.com>
2022-03-28 22:04:06 -0700

    Create 8-print_diagsums

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,3 +3,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+int _putchar(char c);
+void set_string(char **s, char *to);
